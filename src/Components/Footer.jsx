f28e81d40364eced68d5962e93054205369e88a6
Lani23 <linalmg23@gmail.com>
2024-12-07 07:15:20 -0500

    cambio imagenes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,8 +3,8 @@ const Footer = () => {
     <footer className="footer">
       <div className="powered-by">
         <p>Powered by</p>
-        <img src="../../public/images/DH.png" alt="DH-logo" />
-        <img src="../../public/images/ico-facebook.png" alt="Facebook" />
+        <img src="/images/DH.png" alt="DH-logo" />
+        <img src="/images/ico-facebook.png" alt="Facebook" />
         <img src="../../public/images/ico-instagram.png" alt="Instagram" />
         <img src="../../public/images/ico-tiktok.png" alt="TikTok" />
         <img src="../../public/images/ico-whatsapp.png" alt="WhatsApp" />
